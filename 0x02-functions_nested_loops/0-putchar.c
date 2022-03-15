ed994c2f068527cfd60f7349e47799b3ee7e6188
iandan77 <iandan874@gmail.com>
2022-03-15 21:42:36 +0300

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,32 @@
-#include<stdio.h>
+#include <main.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *message is putchar
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+   int str[]={95, 112, 117, 99, 104, 97, 114};
+   int count, sz;
+   sz = sizeof(str)/ sizeof(int);
+   for (count=o; count<sz; count++)
+   {
+      _putchar(str[count]);
+   }
+   _putchar('\n');
+   return (0);
+}
+#include <_putchar.h>
+/**
+ *message is putchar
+ */
+int main(void)
+{
+   int str[]={95, 112, 117, 99, 104, 97, 114};
+   int count, sz;
+   sz = sizeof(str)/ sizeof(int);
+   for (count=o; count<sz; count++)
+   {
+      _putchar(str[count]);
+   }
+   _putchar('\n');
+   return (0);
 }
